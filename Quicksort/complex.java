675399d7852a6074563ce40c4b90f1f7e67e1d3b
jkjiayi <kakala2002@gmail.com>
2021-11-15 13:55:06 +0000

    finished JUnit tests

--- Quicksort/complex.java
+++ Quicksort/complex.java
@@ -1,4 +1,6 @@
 package Quicksort;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.text.DecimalFormat;
 import java.text.NumberFormat;
 import java.util.ArrayList;
@@ -18,12 +20,12 @@ import java.util.stream.IntStream;
 // split it into pairs - smaller arrays and check if they are sorted
 // 
 
-public class complex {
+public class Complex {
     public List<Integer> list = new ArrayList<Integer>();
     public int upperb;
     //Integer[] arr = new Integer[] {3,4,2,1,5,6,7,8,9};
 
-    public complex(int n, int upperb) {
+    public Complex(int n, int upperb) {
         this.upperb = upperb;
         int[]  randomIntsArray = new Random().ints(n, 0, upperb).toArray();
         this.list = Arrays.stream(randomIntsArray).boxed().collect(Collectors.toList());
@@ -33,21 +35,91 @@ public class complex {
     public static void main(String[] args) {
         //List<Integer> myList = Arrays.asList(5, 3, 8, 2, 9, 10, 11, 4, 4, 17, 2, 4, 78, 9, 3, 1, 5, 7, 9, 46 ,1, 6, 9, 4, 2, 3);
         
-        complex obj = new complex(9, 101);
-        System.out.println(obj.list);
-
-        float sort = measureSort(obj.list, obj.upperb);
-        System.out.format("This is how sorted the array is: %f\n", sort);
-        //System.out.println(obj.list);
-        long start = System.currentTimeMillis();
+        // get the main 9 numbers and sort them
+        Complex obj = new Complex(10000, 101);
+        
+        int size = obj.list.size();
         obj.list = sort(obj.list);
-        long end = System.currentTimeMillis();
-        //System.out.println(obj.list);
+        List<Integer> to_pass = obj.list;
+        try {
+            FileWriter writer = new FileWriter("data.csv");
+        
 
-        // if array size was odd, delete the last element
+            for (int i=0; i<size-1; i++){
+        
+                float sort = measureSort(obj.list, obj.upperb);
+                System.out.format("This is how sorted the array is: %f\n", sort);
 
-        NumberFormat formatter = new DecimalFormat("#0.00000");
-        System.out.print("Execution time is " + formatter.format((end - start) / 1000d) + " seconds");
+                // measure time taken
+                long start = System.currentTimeMillis();
+                to_pass = sort(to_pass);
+                long end = System.currentTimeMillis();
+
+
+                NumberFormat formatter = new DecimalFormat("#0.00000");
+                System.out.print("Execution time is " + formatter.format((end - start) / 1000d) + " seconds");
+                System.out.println();
+
+                writeToCSV(writer, sort, end-start);
+                
+                // do interleave for arrays
+                List<List<Integer>> parts = new ArrayList<List<Integer>>();
+                if (size %2 == 0){
+                    parts.add(new ArrayList<Integer>(obj.list.subList(0, (size/2))));
+                    parts.add(new ArrayList<Integer>(obj.list.subList((size/2), size)));
+                } else {
+                    parts.add(new ArrayList<Integer>(obj.list.subList(0, (size/2)+1)));
+                    parts.add(new ArrayList<Integer>(obj.list.subList((size/2)+1, size)));
+                }
+
+                obj.list.clear();
+
+                while (true){
+                    if (parts.get(0).size()==1){
+                        obj.list.add(parts.get(0).get(0));
+                        if (parts.get(1).isEmpty()){
+                            break;
+                        } else {
+                            obj.list.add(parts.get(1).get(0));
+                            break;
+                        }
+                    }
+                    obj.list.add(parts.get(0).get(0));
+                    parts.get(0).remove(0);
+                    obj.list.add(parts.get(1).get(0));
+                    parts.get(1).remove(0);
+                }
+
+                // System.out.println("A");
+                // System.out.println(obj.list);
+
+                int num_shuffle =obj.list.remove(0);
+                obj.list.add(num_shuffle);
+
+                to_pass = obj.list;
+                // System.out.println("B");
+                // System.out.println(obj.list);
+        
+            }
+            writer.close();
+        } catch (IOException e) {
+            System.err.println(e);
+        }
+
+        
+    }
+
+    public static void writeToCSV(FileWriter wr, float sort, float time){
+        StringBuilder sb = new StringBuilder();
+        sb.append(sort);
+        sb.append(',');
+        sb.append(time);
+        sb.append('\n');
+        try {
+            wr.write(sb.toString());
+        } catch (IOException e) {
+            System.out.println(e);
+        }
     }
 
     public static float measureSort(List<Integer> myList, int upperb) {
@@ -58,14 +130,23 @@ public class complex {
         float calc1;
         float calc2;
 
+        // return -1 as error for sorting array with zero elements
+        if (myList.size() == 0){
+            return -1;
+        } else if (myList.size() ==1) { // return 100 as fully sorted for array with only one element
+            return 100;
+        }
+
         // if the size is odd, add an extra number to make a pair
         // do this because comparing the original last element with a number > upperbound will always true
         if ((myList.size() % 2) == 1){
             myList.add(upperb+1);
             isOdd = true;
+            // System.out.println("New array if odd:");
+            // System.out.println(myList);
         }
 
-        System.out.println(myList);
+        
 
         // test 1: test each pair to see if its sorted within themselves
         for (int i=0; i<myList.size(); i= i+2){
@@ -75,9 +156,9 @@ public class complex {
         }
 
         calc1 = ((float)sortedWithinPair)/(myList.size()/2);
-        System.out.println("Test 1");
-        System.out.println(sortedWithinPair);
-        System.out.println(calc1);
+        // System.out.println("Test 1");
+        // System.out.println(sortedWithinPair);
+        // System.out.println(calc1);
 
 
         // test 2: test two pairs to see if they are in the right order
@@ -85,9 +166,9 @@ public class complex {
         List<List<Integer>> parts = new ArrayList<List<Integer>>();
         for (int i = 0; i < myList.size(); i += 2) {
             parts.add(new ArrayList<Integer>(myList.subList(i, i+2)));
-            System.out.println(myList.subList(i, i+2));
+            //System.out.println(myList.subList(i, i+2));
         }
-        System.out.println(parts);
+        //System.out.println(parts);
         
         // compare the largest value of the first pair with the smallest value of the other pair
         List<Integer> array1 = new ArrayList<Integer>();
@@ -97,15 +178,15 @@ public class complex {
             array1 = parts.get(j);
             array2 = parts.get(j+1);
 
-            if (Collections.max(array1) < Collections.min(array2)){
+            if (Collections.max(array1) <= Collections.min(array2)){
                 sortedBetweenPair++;
             }
         }
 
         calc2 = ((float)sortedBetweenPair)/((myList.size()/2) -1);
-        System.out.println("Test 2");
-        System.out.println(sortedBetweenPair);
-        System.out.println(calc2);
+        // System.out.println("Test 2");
+        // System.out.println(sortedBetweenPair);
+        // System.out.println(calc2);
 
         //if the size was previously odd, remove the last element
         if (isOdd == true){
--- Test/Tests.java
+++ Test/Tests.java
@@ -1,37 +1,304 @@
 package Test;
 import org.junit.Test;
 
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import Quicksort.complex;
+import Quicksort.Complex;
 
 
 public class Tests {
-    
+
+    //! check that instantiating new Complex object creates a list (list object is not null)
     @Test
-    public void quickSortWorking() {
-        complex obj = new complex(10, 10);
-        float sort = complex.measureSort(obj.list, obj.upperb);
-
-        // get the sorted array by calling collections
-        List<Integer> originalList = obj.list;
-        Collections.sort(originalList);
-
-        // get sorted array from own code
-        obj.list = complex.sort(obj.list);
-
-        // check if equals
-        int i = 0;
-        while (i<obj.list.size()){
-            assertEquals(originalList.get(i), obj.list.get(i));
-            i++;
-        }
+    public void newComplexGeneratesArrayOfCorrectZeroANdNotNull() {
+        int size = 0;
+        Complex obj = new Complex(size, 10);
+        assertEquals(0, obj.list.size()); 
+        assertNotNull(obj.list);
     }
 
-    @Test 
+    //! check that instantiating new Complex object creates a list of correct odd size upon request
+    @Test
+    public void newComplexGeneratesArrayOfCorrectSizeOdd() {
+        int size = 5;
+        Complex obj = new Complex(size, 10);
+        assertEquals(5, obj.list.size()); 
+    }
+
+    //! check that instantiating new Complex object creates a list of correct even size upon request
+    @Test
+    public void newComplexGeneratesArrayOfCorrectSizeEven() {
+        int size = 6;
+        Complex obj = new Complex(size, 10);
+        assertEquals(6, obj.list.size()); 
+    }
+
+    //! check if numbers within their pair is sorted and calculated correctly - keep test 2 (always 100%)
+    // for last element, a temporary element that MUST be larger than it will added to the end of the list
+    // since the temp is alwayts larger, the last pair for odd size will always be sorted
+    //!  same results for both test1 odd and even tests
+    @Test
+    public void checkTest1WithOddSize(){
+        // [2, 1, 3, 4, 5, 6, 7, 8, 9] // 4/5 pairs sorted // 4/4 - 100% // total - 4/5*5/5 = 80%
+        List<Integer> initialisedList =  Arrays.asList(2, 1, 3, 4, 5, 6, 7, 8, 9);
+        Complex obj = new Complex(10, 10);
+        obj.list.clear();
+
+        for (int i = 0; i<initialisedList.size(); i++){
+            obj.list.add(initialisedList.get(i));
+        }
+        
+        //obj.list = initialisedList;
+        float sort = Complex.measureSort(obj.list, obj.upperb);
+        float correctSortedness = (((float) 4)/5)*100;
+        assertEquals(sort, correctSortedness);
+    }
+
+    //! check if numbers within their pair is sorted and calculated correctly - keep test 2 (always 100%)
+    //!  same results for both test1 odd and even tests
+    @Test
+    public void checkTest1WithEvenSize(){
+        // [2, 1, 3, 4, 5, 6, 7, 8, 9, 10] // 4/5 pairs sorted // 4/4 - 100% // total - 4/5*5/5 = 80%
+        List<Integer> initialisedList =  Arrays.asList(2, 1, 3, 4, 5, 6, 7, 8, 9, 10);
+        Complex obj = new Complex(10, 10);
+        obj.list.clear();
+
+        for (int i = 0; i<initialisedList.size(); i++){
+            obj.list.add(initialisedList.get(i));
+        }
+        
+        //obj.list = initialisedList;
+        float sort = Complex.measureSort(obj.list, obj.upperb);
+        float correctSortedness = (((float) 4)/5)*100;
+        assertEquals(sort, correctSortedness);
+    }
+
+    //! check if numbers between pairs are sorted and calculated correctly - keep test 1 (always 100%)
+    // test 2 compares the LARGEST element of the first pair with the SMALLEST element of the second pair
+    //  temporary element that MUST be larger than it will added to the end of the list
+    //  but since the temp is always larger, the actual last element will always be used to compare with the pervious pair
+    //  therefore adding the temporary last element will not affect results
+    //!  same results for both test2 odd and even tests
+    @Test
+    public void checkTest2WithOddSize(){
+        // [1, 2, 5, 6, 3, 4, 7, 8, 9, 10] // 5/5 pairs sorted // 3/4 - 100% // total - 5/5*3/4 = 75%
+        List<Integer> initialisedList =  Arrays.asList(1, 2, 3, 4, 5, 6, 7, 9, 8);
+        Complex obj = new Complex(10, 10);
+        obj.list.clear();
+
+        for (int i = 0; i<initialisedList.size(); i++){
+            obj.list.add(initialisedList.get(i));
+        }
+        
+        //obj.list = initialisedList;
+        float sort = Complex.measureSort(obj.list, obj.upperb);
+        float correctSortedness = (((float) 3)/4)*100;
+        assertEquals(sort, correctSortedness);
+    }
+
+    //! check if numbers between pairs are sorted and calculated correctly - keep test 1 (always 100%)
+    // test 2 compares the LARGEST element of the first pair with the SMALLEST element of the second pair
+    //!  same results for both test2 odd and even tests
+    @Test
+    public void checkTest2WithEvenSize(){
+        // [1, 2, 5, 6, 3, 4, 7, 8, 9] // 5/5 pairs sorted // 3/4 - 100% // total - 5/5*3/4 = 75%
+        List<Integer> initialisedList =  Arrays.asList(1, 2, 3, 4, 5, 6, 7, 9, 8, 10);
+        Complex obj = new Complex(10, 10);
+        obj.list.clear();
+
+        for (int i = 0; i<initialisedList.size(); i++){
+            obj.list.add(initialisedList.get(i));
+        }
+        
+        //obj.list = initialisedList;
+        float sort = Complex.measureSort(obj.list, obj.upperb);
+        float correctSortedness = (((float) 3)/4)*100;
+        assertEquals(sort, correctSortedness);
+    }
+
+    //! list with no elements is returns sortedness value of -1
+    @Test
+    public void checkMeasureSortWithSizeZero() {
+        int size = 0;
+        Complex obj = new Complex(size, 10);
+
+        float sort = Complex.measureSort(obj.list, obj.upperb);
+        float correctSortedness = -1;
+        assertEquals(sort, correctSortedness);
+    }
+
+    //! list with only one element is fully sorted (100%)
+    @Test
+    public void checkMeasureSortWithSizeOne() {
+        int size = 1;
+        Complex obj = new Complex(size, 10);
+
+        float sort = Complex.measureSort(obj.list, obj.upperb);
+        float correctSortedness = 100;
+        assertEquals(sort, correctSortedness);
+    }
+
+    //! check if sortedness is correct for odd array size
+    @Test
+    public void checkMeasureSortWithOddSize(){
+        // [3, 4, 2, 1, 5, 6, 7, 8, 9, 10] // 4/5 pairs sorted // 2/4 - 50% // total - 2/5 - 40%
+        List<Integer> initialisedList =  Arrays.asList(3, 4, 2, 1, 5, 6, 9, 10, 7);
+        Complex obj = new Complex(10, 10);
+        obj.list.clear();
+
+        for (int i = 0; i<initialisedList.size(); i++){
+            obj.list.add(initialisedList.get(i));
+        }
+        
+        //obj.list = initialisedList;
+        float sort = Complex.measureSort(obj.list, obj.upperb);
+        float correctSortedness = (((float) 2)/5)*100;
+        assertEquals(sort, correctSortedness);
+        
+    }
+    //! check if sortedness is correct for even array size
+    @Test
+    public void checkMeasureSortWithEvenSize(){
+        // [3, 4, 2, 1, 5, 6, 9, 10, 7, 8] // 4/5 pairs sorted // 2/4 - 50% // total - 2/5 - 40%
+        List<Integer> initialisedList =  Arrays.asList(3, 4, 2, 1, 5, 6, 9, 10, 7, 8);
+        Complex obj = new Complex(10, 10);
+        obj.list.clear();
+
+        for (int i = 0; i<initialisedList.size(); i++){
+            obj.list.add(initialisedList.get(i));
+        }
+        
+        //obj.list = initialisedList;
+        float sort = Complex.measureSort(obj.list, obj.upperb);
+        float correctSortedness = (((float) 2)/5)*100;
+        assertEquals(sort, correctSortedness);
+    }
+
+    //! check if sortedness is correct for increasing (fully) sorted array of any size
+    @Test
+    public void checkMeasureSortWithSortedArray(){
+        // [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] // 5/5 pairs sorted // 4/4 - 100% // total - 1/1 - 100%
+        List<Integer> initialisedList =  Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+        Complex obj = new Complex(10, 10);
+        obj.list.clear();
+
+        for (int i = 0; i<initialisedList.size(); i++){
+            obj.list.add(initialisedList.get(i));
+        }
+        
+        //obj.list = initialisedList;
+        float sort = Complex.measureSort(obj.list, obj.upperb);
+        float correctSortedness = (((float) 1)/1)*100;
+        assertEquals(sort, correctSortedness);
+    }
+
+    //! check if sortedness is correct for decreasing sorted array of any size
+    @Test
+    public void checkMeasureSortWithOppSortedArray(){
+        // [10, 9, 8, 7, 6, 5, 4, 3, 2, 1] // 0/5 pairs sorted // 0/4 - 0% // total - 0/0 - 0%
+        List<Integer> initialisedList =  Arrays.asList(10, 9, 8, 7, 6, 5, 4, 3, 2, 1);
+        Complex obj = new Complex(10, 10);
+        obj.list.clear();
+
+        for (int i = 0; i<initialisedList.size(); i++){
+            obj.list.add(initialisedList.get(i));
+        }
+        
+        //obj.list = initialisedList;
+        float sort = Complex.measureSort(obj.list, obj.upperb);
+        float correctSortedness = 0;
+        assertEquals(sort, correctSortedness);
+    }
+
+    //! check if sortedness is correct for array of any size with all the same elements
+    @Test
+    public void checkMeasureSortIdenticalArray(){
+        // [3, 3, 3, 3, 3, 3, 3, 3, 3, 3] // 5/5 pairs sorted // 4/4 - 100% // total - 1/1 - 100%
+        List<Integer> initialisedList =  Arrays.asList(3, 3, 3, 3, 3, 3, 3, 3, 3, 3);
+        Complex obj = new Complex(10, 10);
+        obj.list.clear();
+
+        for (int i = 0; i<initialisedList.size(); i++){
+            obj.list.add(initialisedList.get(i));
+        }
+        
+        //obj.list = initialisedList;
+        float sort = Complex.measureSort(obj.list, obj.upperb);
+        float correctSortedness = (((float) 1)/1)*100;
+        assertEquals(sort, correctSortedness);
+    }
+
+    //! check if my quicksort is correct for array of size zero by comparing it with the collection sort function
+    @Test
+    public void sizeZeroSort(){
+        Complex obj = new Complex(0, 10);
+
+        // get the sorted array by calling collections
+        List<Integer> collectionSortedList = obj.list;
+        Collections.sort(collectionSortedList);
+
+        // get sorted array from own code
+        obj.list = Complex.sort(obj.list);
+
+        // check if equals
+        
+        assertTrue(collectionSortedList.equals(obj.list));
+    }
+
+    //! check if my quicksort is correct for array of size odd size by comparing it with the collection sort function
+    public void randomSizeOfOdd() {
+        Complex obj = new Complex(5, 10);
+        //float sort = Complex.measureSort(obj.list, obj.upperb);
+
+        // get the sorted array by calling collections
+        List<Integer> collectionSortedList = obj.list;
+        Collections.sort(collectionSortedList);
+
+        // get sorted array from own code
+        obj.list = Complex.sort(obj.list);
+
+        // check if equals
+        assertTrue(collectionSortedList.equals(obj.list));
+
+        // int i = 0;
+        // while (i<obj.list.size()){
+        //     assertEquals(collectionSortedList.get(i), obj.list.get(i));
+        //     i++;
+        // }
+    }
+
+    //! check if my quicksort is correct for array of even size by comparing it with the collection sort function
+    @Test
+    public void randomSizeOfEven(){
+        Complex obj = new Complex(6, 10);
+
+        // get the sorted array by calling collections
+        List<Integer> collectionSortedList = obj.list;
+        Collections.sort(collectionSortedList);
+
+        // get sorted array from own code
+        obj.list = Complex.sort(obj.list);
+
+        // check if equals
+        assertTrue(collectionSortedList.equals(obj.list));
+
+        // int i = 0;
+        // while (i<obj.list.size()){
+        //     assertEquals(collectionSortedList.get(i), obj.list.get(i));
+        //     i++;
+        // }
+    }
+
+    
 }
